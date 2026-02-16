9c5755adbe08dc5cdb84ee42b6f0b556a6355b9a
Myeongjae Seo <peterseo1004@icloud.com>
2026-02-15 19:41:55 -0600

    got the flexbox working, now time for some Javascript linking

---
+++ main.js
@@ -0,0 +1,14 @@
+let GameManualRedirect = document.getElementById("GameManualRedirect"); 
+GameManualRedirect.addEventListener("click", ()=> {
+    window.location.href = "2026GameManual.pdf";
+});
+
+let ArticleDirectoryRedirect = document.getElementById("ArticleDirectoryRedirect"); 
+GameManualRedirect.addEventListener("click", ()=> {
+    window.location.href = "2026GameManual.pdf";
+});
+
+
+
+
+
--- scrollingTitle.js
+++ scrollingTitle.js
@@ -1,27 +1,12 @@
-var title = document.getElementById("title");
+var title = document.getElementById("titleName");
 
-setTimeout(() => {console.log("halleigluha");
-
-
-
-}, 100);
+setTimeout(() => {console.log("halleigluha");}, 100);
 
 
 changeTitle = function(){
    var letter = title[0];
    blurPageTitle = title.substr(1) + letter;
    title = title;
-   changeTitleTimer = setTimeout(changeTitle, 500);
+   changeTitleTimer = setTimeout(changeTitle(), 500);
 }
 
-
-
-
-/*
-'''
-while (true){
-
-
-}
-'''
-*/
